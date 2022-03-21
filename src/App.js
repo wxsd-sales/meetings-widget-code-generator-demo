6d20468436cca2269028e6ea443ae5185fb497f3
Nivetha Jayakumar <nivethajayakumar5@gmail.com>
2022-03-20 23:35:08 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,174 @@
+import React, {useState} from 'react';
+import {Select, SelectOption, Input, Checkbox, CheckboxGroup} from '@momentum-ui/react';
+import '@momentum-ui/core/css/momentum-ui.min.css';
+import './App.css';
+
+export default function App() {
+  
+  return (
+      <div>
+          <header className='App-header'>
+          {/* <h1 className='header'>Meetings Widget Demo</h1> */}
+          <div className='outerDiv'>
+                <div className='flex-child'>
+                    left div
+                    <div className='form-inline'>
+                    
+                    <Input
+                        name='Access Token'
+                        label='Access Token'
+                        htmlId='defaultInput'
+                        inputSize='small-10'
+                        placeholder='access token'
+                    />
+                    <Input
+                        name='Meeting Destination'
+                        label='Meeting Destination'
+                        htmlId='defaultInput'
+                        inputSize='small-10'
+                        placeholder='meeting destination ID'
+                    />
+                    
+                    </div>
+                    <div className='form-inline'>
+                        <div>
+                            
+                        InMeeting Controls
+                            <div className='form-inline'>
+                                <div>
+                                    <CheckboxGroup name='CheckboxGroup1'>
+                                        <Checkbox
+                                            value='mute-audioInMeeting'
+                                            label='mute-audio'
+                                            htmlId='testCheckbox1'
+                                            onChange={() => { }}
+                                        />
+                                        <Checkbox
+                                            value='mute-videoInMeeting'
+                                            label='mute-video'
+                                            htmlId='testCheckbox2'
+                                            onChange={() => { }}
+                                        />   
+                                    </CheckboxGroup>
+                                </div>
+
+                                <div>
+                                <CheckboxGroup name='CheckboxGroup2'>
+                                        <Checkbox
+                                            value='settingsInMeeting'
+                                            label='settings'
+                                            htmlId='testCheckbox3'
+                                            onChange={() => { }}
+                                        />
+                                        <Checkbox
+                                            value='share-screenInMeeting'
+                                            label='share-screen'
+                                            htmlId='testCheckbox4'
+                                            onChange={() => { }}
+                                        />  
+                                    </CheckboxGroup>
+                                </div>
+
+                                <div>
+                                    <CheckboxGroup name='CheckboxGroup3'>  
+                                        <Checkbox
+                                            value='leave-meetingInMeeting'
+                                            label='leave-meeting'
+                                            htmlId='testCheckbox5'
+                                            onChange={() => { }}
+                                        />
+                                        <Checkbox
+                                            value='member-roasterInMeeting'
+                                            label='member-roaster'
+                                            htmlId='testCheckbox6'
+                                            onChange={() => { }}
+                                        />     
+                                    </CheckboxGroup>
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                    <div className="form-inline">
+                        <div>
+                        Interstitial Controls
+                            <div className="form-inline">
+                                <div>
+                                <CheckboxGroup name='CheckboxGroup4'>
+                                    <Checkbox
+                                        value='mute-audioInterstitial'
+                                        label='mute-audio'
+                                        htmlId='testCheckbox7'
+                                        onChange={() => { }}
+                                    />
+                                    <Checkbox
+                                        value='mute-videoInterstitial'
+                                        label='mute-video'
+                                        htmlId='testCheckbox8'
+                                        onChange={() => { }}
+                                    />
+                                </CheckboxGroup>
+
+                                </div>
+                                <div>
+                                    <CheckboxGroup name='CheckboxGroup5'>
+                                        <Checkbox
+                                            value='settingsInterstitial'
+                                            label='settings'
+                                            htmlId='testCheckbox9'
+                                            onChange={() => { }}
+                                        />
+                                        <Checkbox
+                                            value='join-meetingInterstitial'
+                                            label='join-meeting'
+                                            htmlId='testCheckbox10'
+                                            onChange={() => { }}
+                                        />   
+                                    </CheckboxGroup>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        Theme
+                        <div className='form-inline'>   
+                            <Select defaultValue='Select Item Here' >
+                                <SelectOption value='light' label='light' />
+                                <SelectOption value='dark' label='dark' />
+                            </Select>
+                        </div>
+                    </div> 
+                    <div>
+                        Layout
+                        <div >
+                            <Input
+                                name='width'
+                                label='Width'
+                                htmlId='defaultInput3'
+                                inputSize='small-2'
+                                placeholder='width'
+                            />
+                            <Input
+                                name='height'
+                                label='Height'
+                                htmlId='defaultInpu4'
+                                inputSize='small-2'
+                                placeholder='height'
+                            />
+                        </div>
+                    </div>
+                    
+                    
+                    
+                    
+                    
+                </div>
+                <div className='flex-child'>
+                    right div
+                    
+              </div>
+          </div>
+          </header>
+      </div>
+  );
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const webpack = require('webpack');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {version} = require('./package.json');
+
+module.exports = function(env, argv) {
+  return {
+    cache: true,
+    mode: 'development',
+    output:
+      argv.mode === 'production'
+        ? {
+            path: path.resolve(__dirname, './build'),
+            filename: 'bundle.js',
+          }
+        : undefined, // Otherwise the CleanWebpackPlugin will wipe our build during devserver
+    resolve: {
+        fallback: {
+            fs: false
+        }
+    },
+    devtool: argv.mode === 'production' ? 'source-map' : 'inline-source-map',
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg|png|gif)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'assets/',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    devServer:
+      argv.mode === 'development'
+        ? {
+            contentBase: path.resolve(__dirname, './src'),
+            open: true,
+            overlay: true,
+            hot: true,
+            port: 9000,
+            stats: 'errors-warnings',
+            https: true,
+          }
+        : undefined,
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'public/index.html',
+        favicon: 'public/webex-logo.png',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        __appVersion__: JSON.stringify(version)
+      })
+    ],
+  };
+};
